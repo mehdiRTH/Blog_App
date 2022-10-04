0116bd8774148e0cc53cc3247acf950e17efb3eb
mehdiRTH <tayarthmehdi@gmail.com>
2022-10-04 09:34:29 +0100

    first and completed commit

---
+++ database/migrations/2022_10_03_152519_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('image');
+            $table->longText('content');
+            $table->foreignId('tags')->constrained('tags');
+            $table->foreignId('categorie')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
